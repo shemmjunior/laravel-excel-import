d94e3ef2042577650bcd792f13c1800f1ec682eb
Mgini <juniorshemm@gmail.com>
2022-06-10 12:05:37 +0300

    refactor code in view imports table

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,45 +1,45 @@
 <x-layout>
     @unless(count($cargos) == 0)
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg lg:mx-24">
-        <table class="w-full text-sm text-left dark:text-gray-400">
-            <thead class="text-xs uppercase bg-gray-50 dark:bg-gray-700">
-                <tr>
-                    <th scope="col" class="pl-3 py-3">
-                        Cargo no
-                    </th>
-                    <th scope="col" class="py-3">
-                        Cargo type
-                    </th>
-                    <th scope="col" class="py-3">
-                        Cargo size
-                    </th>
-                    <th scope="col" class="py-3">
-                        Weight (Kg)
-                    </th>
-                    <th scope="col" class="py-3">
-                        Remarks
-                    </th>
-                    <th scope="col" class="py-3">
-                        Wharfage (USD)
-                    </th>
-                    <th scope="col" class="py-3">
-                        Penalty (Days)
-                    </th>
-                    <th scope="col" class="py-3">
-                        Storage (USD)
-                    </th>
-                    <th scope="col" class="py-3">
-                        Electricity (USD)
-                    </th>
-                    <th scope="col" class="py-3">
-                        Destuffing (USD)
-                    </th>
-                    <th scope="col" class="py-3">
-                        Lifting (USD)
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg lg:mx-24">
+            <table class="w-full text-sm text-left dark:text-gray-400">
+                <thead class="text-xs uppercase bg-gray-50 dark:bg-gray-700">
+                    <tr>
+                        <th scope="col" class="pl-3 py-3">
+                            Cargo no
+                        </th>
+                        <th scope="col" class="py-3">
+                            Cargo type
+                        </th>
+                        <th scope="col" class="py-3">
+                            Cargo size
+                        </th>
+                        <th scope="col" class="py-3">
+                            Weight (Kg)
+                        </th>
+                        <th scope="col" class="py-3">
+                            Remarks
+                        </th>
+                        <th scope="col" class="py-3">
+                            Wharfage (USD)
+                        </th>
+                        <th scope="col" class="py-3">
+                            Penalty (Days)
+                        </th>
+                        <th scope="col" class="py-3">
+                            Storage (USD)
+                        </th>
+                        <th scope="col" class="py-3">
+                            Electricity (USD)
+                        </th>
+                        <th scope="col" class="py-3">
+                            Destuffing (USD)
+                        </th>
+                        <th scope="col" class="py-3">
+                            Lifting (USD)
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
                     @foreach ($cargos as $cargo)
                         <tr class="bg-white border-b dark:border-gray-700">
                             <td class="pl-3 py-4">
@@ -77,14 +77,10 @@
                             </td>
                         </tr>
                     @endforeach
+                </tbody>
+            </table>
         </div>
     @else
-        <p class="text-center">No cargos added</p>
+        <p class="text-center">No cargos added yet!</p>
     @endunless
-
-
-    </tbody>
-    </table>
-    </div>
-
 </x-layout>
