074d7760ab15af918bd42368de538e359a3b5d1f
Mgini <juniorshemm@gmail.com>
2022-06-10 09:58:57 +0300

    add form to import excel file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,31 @@
 <x-layout>
-    
+    <div class="flex justify-center items-center h-screen">
+        <div class="mx-44">
+            <form method="POST">
+                @csrf
+                <div class="grid grid-cols-2 gap-4">
+                    <div>
+                        <input type="file" name="excel-file"
+                            class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                            aria-describedby="file_input_help" id="file_input">
+                            @error('excel-file')
+                            <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+                                {{ $message }}
+                            </p>
+                            @enderror
+                    </div>
+                  <div>
+                    <button type="submit"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    Submit
+                </button>
+                  </div>
+ 
+                </div>
+
+                </div>
+            </form>
+        </div>
+
+    </div>
 </x-layout>
