ef62899599c147cb2f53e9e4e660f68abe0290bd
Mgini <juniorshemm@gmail.com>
2022-06-19 16:52:10 +0300

    Update Car.php

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
+/**
+ * https://www.codecheef.org/article/tracking-user-activities-using-laravel-audit-package
+ * https://laravel-auditing.com/docs/13.0/model-setup
+ */
 
 class Car extends Model implements Auditable
 {
